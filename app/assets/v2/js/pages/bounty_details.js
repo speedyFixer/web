99464d610c5a24618e8afe1963ca9800ffa7df14
Octavio Amuchástegui <octavioamuchastegui@gmail.com>
2019-04-16 19:54:36 -0300

    remove return false

--- app/assets/v2/js/pages/bounty_details.js
+++ app/assets/v2/js/pages/bounty_details.js
@@ -458,9 +458,7 @@ var isBountyOwner = function(result) {
   if (typeof web3 == 'undefined') {
     return false;
   }
-  if (typeof web3.eth == 'undefined') {
-    return false;
-  }
+  
   if (typeof web3.eth.coinbase == 'undefined' || !web3.eth.coinbase) {
     return false;
   }
